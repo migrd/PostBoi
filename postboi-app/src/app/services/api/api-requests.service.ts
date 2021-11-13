e1ad4f44238cbe98c5719220f8fb322aa5a50421
migrd <miguel.hwe@gmail.com>
2021-11-13 20:49:39 -0300

    simple get
    added simple get in api service

--- postboi-app/src/app/services/api/api-requests.service.ts
+++ postboi-app/src/app/services/api/api-requests.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiRequestsService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  getPersonagem(apiUrl: string): Observable<any>{ //simple get
+    return this._http.get<any>(apiUrl);
+  }
 }
